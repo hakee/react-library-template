25b366c73ec66a61f55e058d87f20fbc4181392a
Bogdan Bias <hakee@haktos.com>
2023-04-11 18:45:56 +0300

    Barebone config

---
+++ rollup.config.js
@@ -0,0 +1,44 @@
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import resolve from '@rollup/plugin-node-resolve'
+import commonJS from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import { terser } from 'rollup-plugin-terser'
+import dts from 'rollup-plugin-dts'
+import styles from 'rollup-plugin-styles'
+
+const packageJson = require('./package.json')
+
+export default [
+  {
+    input: 'src/index.ts',
+    cache: false,
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    treeshake: false,
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonJS(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      terser(),
+      styles(),
+    ],
+    external: ['react', 'react-dom'],
+  },
+  {
+    input: 'dist/esm/types/index.d.ts',
+    // input: 'dist/esm/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts(), styles()],
+  },
+]
